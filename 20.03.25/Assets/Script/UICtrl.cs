c46b3370c4ac804f745db80ff4f221c092719124
piche32 <piche32@naver.com>
2020-04-30 18:33:43 +0900

    Chicken Run! UI
    Add UI and tutorial text

---
+++ 20.03.25/Assets/Script/UICtrl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICtrl : MonoBehaviour
+{
+    [SerializeField] GameObject tutorial;
+    // Start is called before the first frame update
+
+    AudioSource audioSource;
+    [SerializeField] AudioClip openSound = null;
+    [SerializeField] AudioClip closeSound = null;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            if (!tutorial.activeSelf)
+            {
+                tutorial.SetActive(true);
+                audioSource.clip = openSound;
+                audioSource.Play();
+            }
+            else
+            {
+                tutorial.SetActive(false);
+                audioSource.clip = closeSound;
+                audioSource.Play();
+            }
+        }
+    }
+}
